c2aa21ee6fb0dc86cae641d35a73641b4add7da5
Thomas LUK <thomasluk624@gmail.com>
2023-09-09 04:04:06 +0800

    feat(receiver): now can receive message from server as a string and publish it in ros2 topic /from_websocket
    Signed-off-by: Thomas LUK <thomasluk624@gmail.com>

--- ros2websocket/ros2websocket_receiver_node.py
+++ ros2websocket/ros2websocket_receiver_node.py
@@ -16,6 +16,7 @@ class WebsocketReceiver(Node):
         self.connection_state_pub = self.create_publisher(Bool, '/charging_robot/connection_state', 10)
         timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        
         self.uri = 'ws://localhost:8000/ws/001'
         self.connection_state = False
 
@@ -25,26 +26,33 @@ class WebsocketReceiver(Node):
         self.connection_state_pub.publish(msg)
 
     async def recv_from_websocket(self, websocket):
-        try:
-            async for message in websocket:
-                await self.websocket_to_ros_pub.publish(message)
-                self.get_logger().info('Successfully receive message from server')
-        except Exception as e:
-            self.get_logger().error(str(e))
-    
-    async def connect_to_server(self):
-        async for websocket in websockets.connect(self.uri):
-            try:
-                self.connection_state = True
-                self.get_logger().info(f'Successful to connect the server {self.uri}')
-                await self.recv_from_websocket(websocket)
-            except websockets.ConnectionClosed:
-                self.connection_state = False
-                self.get_logger().error(f'Fail to connect the server {self.uri}')
+        async for message in websocket:
+            if message is None:
+                self.get_logger().warning(f'Received message is NoneType')
                 continue
+            if not message:
+                self.get_logger().warning(f'Received empty message')
+                continue
+            string_msg = String()
+            string_msg.data = message
+            print(string_msg)
+            self.websocket_to_ros_pub.publish(string_msg)
+            self.get_logger().info('Successfully receive message from server')
 
 
-async def spinning(node):
+async def connect_to_server(node: WebsocketReceiver):
+    async for websocket in websockets.connect(node.uri):
+        try:
+            node.connection_state = True
+            node.get_logger().info(f'Successful to connect the server {node.uri}')
+            await node.recv_from_websocket(websocket)
+        except websockets.ConnectionClosed:
+            node.connection_state = False
+            node.get_logger().error(f'Fail to connect the server {node.uri}')
+            continue
+
+
+async def spinning(node: WebsocketReceiver):
     while rclpy.ok():
         rclpy.spin_once(node, timeout_sec=0.01)
         await asyncio.sleep(0.001)
@@ -55,9 +63,9 @@ async def run(args, loop: asyncio):
     websocket_receiver = WebsocketReceiver()
 
     spin_task = loop.create_task(spinning(websocket_receiver))
-
+    connect_task = loop.create_task(connect_to_server(websocket_receiver))
     try:
-        await spin_task
+        await asyncio.gather(spin_task, connect_task)
     except asyncio.exceptions.CancelledError:
         pass
 
